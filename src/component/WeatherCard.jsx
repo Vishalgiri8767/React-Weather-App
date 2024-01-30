c201063b0c0accca0281ee1125823786701956bc
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-01-30 18:18:45 +0530

    optimized- used only one useState instead of each of seperate piece

--- src/component/WeatherCard.jsx
+++ src/component/WeatherCard.jsx
@@ -1,19 +1,27 @@
 import React, {useEffect, useState } from 'react'
 
 const WeatherCard = () => {
-  const [temp ,setTemp] = useState();
-  const [desc, setDesc] = useState();
-  const [humidity, setHumidity] = useState();
-  const [cityName, setCityName] = useState("");
-  const [country, setCountry] = useState("");
-  const [weatherIcon, setWeatherIcon] = useState("");
+  // const [temp ,setTemp] = useState();
+  // const [desc, setDesc] = useState();
+  // const [humidity, setHumidity] = useState();
+  // const [cityName, setCityName] = useState("");
+  // const [country, setCountry] = useState("");
+  // const [weatherIcon, setWeatherIcon] = useState("");
 
+  const [weatherData, setWeatherData] = useState({
+    temp:"",
+    desc:"",
+    humidity:"",
+    cityName:"",
+    country:"",
+    weatherIcon:""
+  })
   const city = document.getElementsByClassName("cityInput");
   
   const handleSearchCity = ()=>{
     useWeatherApi();
   };
-  
+
   const useWeatherApi = async() =>{
     const api_key = "b33ff06ccbc1eaafca719cd0057b78f1";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city[0].value}&appid=${api_key}`;
@@ -22,18 +30,27 @@ const WeatherCard = () => {
     const weather_data = await data.json();
 
     console.log(weather_data);
-     const tempDeg = (weather_data.main.temp-273.15).toFixed(2);
+    const tempDeg = (weather_data.main.temp-273.15).toFixed(2);
      
-     setTemp(tempDeg);
-     setDesc(weather_data.weather[0].description);
-     const humidity = (weather_data.main.humidity);
-     setHumidity(humidity);
+    //  setTemp(tempDeg);
+    //  setDesc(weather_data.weather[0].description);
+  //    const humidity = (weather_data.main.humidity);
+    //  setHumidity(humidity);
 
-     setCityName(weather_data.name);
-     setCountry(weather_data.sys.country);
+    //  setCityName(weather_data.name);
+    //  setCountry(weather_data.sys.country);
 
-      const icon = (weather_data.weather[0].icon);
-      setWeatherIcon(icon);
+    //   const icon = (weather_data.weather[0].icon);
+    //   setWeatherIcon(icon);
+
+      setWeatherData({
+        temp:tempDeg,
+        desc:weather_data.weather[0].description,
+        humidity:weather_data.main.humidity,
+        cityName: weather_data.name,
+        country:weather_data.sys.country,
+        weatherIcon:weather_data.weather[0].icon,
+      })
   };
 
   useEffect(()=>{
@@ -53,13 +70,13 @@ const WeatherCard = () => {
         </form>
 
       <div className='bg-zinc-500 rounded-md text-white mt-8 p-8 '>
-          <p className='font-semibold'>{cityName+" "+country}</p>
-          <img src={`http://openweathermap.org/img/w/${weatherIcon}.png`}
+          <p className='font-semibold'>{weatherData.cityName+" "+weatherData.country}</p>
+          <img src={`http://openweathermap.org/img/w/${weatherData.weatherIcon}.png`}
               className='w-24 h-24  left-0 right-0 mx-auto ' />
-          <p className='text-center text-xl  '>{desc}</p>
+          <p className='text-center text-xl  '>{weatherData.desc}</p>
 
-          <p className=' text-4xl text-center p-4 '>{temp}<sup><sup>o</sup>c</sup> </p>
-          <p className='text-center text-xl '>Humidity:- {humidity}%</p>
+          <p className=' text-4xl text-center p-4 '>{weatherData.temp}<sup><sup>o</sup>c</sup> </p>
+          <p className='text-center text-xl '>Humidity:- {weatherData.humidity}%</p>
        </div>
   
       </div>
